5a3dce90bea4d1fa86e23e7024730b95784a5acd
tt67wq <alex@mys4s.cn>
2019-08-05 11:18:36 +0800

    mqjjsr6ibm

--- common/lib/crypto.ex
+++ common/lib/crypto.ex
@@ -81,4 +81,32 @@ defmodule Common.Crypto do
     padding_size = :binary.last(message)
     {:ok, binary_part(message, 0, byte_size(message) - padding_size)}
   end
+
+  #### ssl tools
+  defp decode_public(nil), do: nil
+
+  defp decode_public(pem) do
+    [{:Certificate, der_bin, :not_encrypted}] = :public_key.pem_decode(pem)
+    der_bin
+  end
+
+  defp decode_private(pem) do
+    [{type, der_bin, :not_encrypted}] = :public_key.pem_decode(pem)
+    {type, der_bin}
+  end
+
+  @spec load_ssl(Keyword.t()) :: [any()]
+  def load_ssl([]), do: []
+
+  def load_ssl(ssl) do
+    ssl = Enum.into(ssl, %{})
+
+    [
+      cacerts: ssl.ca_cert |> decode_public() |> List.wrap(),
+      cert: ssl.cert |> decode_public(),
+      key: ssl.key |> decode_private()
+    ]
+    |> Enum.reject(fn {_k, v} -> v == nil end)
+  end
+
 end
--- common/lib/format.ex
+++ common/lib/format.ex
@@ -35,10 +35,58 @@ defmodule Common.Format do
       end)
 
   @doc """
+  map 转成 xml
+  """
+  def map2xml(params) do
+    content =
+      params
+      |> Map.to_list()
+      |> Enum.map(fn {k, v} -> "<#{k}>#{v}</#{k}>" end)
+      |> Enum.join()
+
+    "<xml>" <> content <> "</xml>"
+  end
+
+  @doc """
+  xml 转 map
+  """
+  def xml2map(xml) do
+    re = ~r/<(?<k>.+)>(?<v>.+)<\/(.+)>/
+
+    xml
+    |> String.slice(5..-8)
+    |> String.split("\n")
+    |> Enum.map(fn x -> Regex.named_captures(re, x) end)
+    |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, Map.get(x, "k"), cdata(Map.get(x, "v"))) end)
+    |> stringkey2atom()
+  end
+
+  defp cdata(str) do
+    if String.contains?(str, "CDATA") do
+      ~r/\<\!\[CDATA\[(?<v>.*)\]\]\>/
+      |> Regex.named_captures(str)
+      |> Map.get("v")
+    else
+      str
+    end
+  end
+
+  @doc """
   map按照字典序排列并链接
   """
   @spec sort_and_concat(map()) :: String.t()
-  def sort_and_concat(params) do
+  def sort_and_concat(params, case_sensitive \\ false)
+
+  def sort_and_concat(params, true) do
+    params
+    |> Map.to_list()
+    |> Enum.filter(fn {_, y} -> y != "" end)
+    |> Enum.sort(fn {x, _}, {y, _} -> x < y end)
+    |> Enum.map(fn {x, y} -> "#{x}=#{y}" end)
+    |> Enum.join("&")
+  end
+
+  def sort_and_concat(params, false) do
     params
     |> Map.to_list()
     |> Enum.filter(fn {_, y} -> y != "" end)
